7bf050208976d3655c317391e184b2105bbfb8c9
cfj1996 <929762292@qq.com>
2019-05-06 09:30:32 +0800

    5-6早上保存

--- src/config.js
+++ src/config.js
@@ -5,22 +5,24 @@
  */
 
 export default {
-  isProdEnv: false, // 是否是生产环境
+  isProdEnv: true, // 是否是生产环境
 
   // 正式环境
-  // HOST: 'http://xykviph5.isales.tech',
-  // LOGIN_HOST: 'http://120.24.226.70:8080',
+  HOST: 'http://xykvip.winbons.com',
+  LOGIN_HOST: 'http://xykvip.winbons.com',
+  // 微信授权URL
+  wxSignHost: 'http://xykviph5.winbons.com', // 微信分享也是用的这个链接
   // REDIRECT_URL: 'http://shop.qdshsh.com/#',
   // SHARE_ADDRESS: 'http://shop.qdshsh.com/#/userId=',
 
   // 测试环境
-  HOST: 'http://devxykvip.isales.tech',
-  LOGIN_HOST: 'http://120.76.237.100:8080',
-  SHARE_ADDRESS: 'http://shoptest.qdshsh.com/#/userId=',
+  //HOST: 'http://devxykvip.isales.tech',
+  // 微信授权URL
+  //wxSignHost: 'http://devxykviph5.isales.tech', // 微信分享也是用的这个链接
+  //LOGIN_HOST: 'http://120.76.237.100:8080',
+  //SHARE_ADDRESS: 'http://shoptest.qdshsh.com/#/userId=',
 
   // ==================  生产与开发通用环境变量  ==============================
-  // 微信授权URL
-  wxSignHost: 'http://devxykviph5.isales.tech', // 微信分享也是用的这个链接
   h5AppId: 100, // 应用标识ID（与后端约定）
   appId: 'wx97a3f8c05002bd41', // 微信appId
   qiniuUpUrl: 'http://upload.qiniu.com/', // 上传域名
